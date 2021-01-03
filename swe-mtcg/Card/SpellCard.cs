22af5d41a374df94d0727fc1d748b4e1f7fe839e
David Zelenay <david.zelenay@tietoevry.com>
2021-01-03 14:20:01 +0100

    Added GetEffectivenessMultiplier to SpellCard.cs

--- swe-mtcg/Card/SpellCard.cs
+++ swe-mtcg/Card/SpellCard.cs
@@ -1,13 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace swe_mtcg.Card
+﻿namespace swe_mtcg.Card
 {
     public class SpellCard : AbstractCard
     {
-        public SpellCard(string name, int damage, CardElement element = CardElement.Normal) : base(name, damage, element, CardType.SpellCard)
+        public SpellCard(string name, double damage, CardElement element = CardElement.Normal, string id = "") : base(
+            name, damage, element, id)
         {
+        }
+
+        public double GetEffectivenessMultiplier(ICard other)
+        {
+            if (this.Element == other.Element)
+            {
+                return 1;
+            }
+
+            if ((this.Element == CardElement.Water && other.Element == CardElement.Fire) ||
+                (this.Element == CardElement.Fire && other.Element == CardElement.Normal) ||
+                (this.Element == CardElement.Normal && other.Element == CardElement.Water))
+            {
+                return 2;
+            }
+
+            return 0.5;
         }
     }
 }
