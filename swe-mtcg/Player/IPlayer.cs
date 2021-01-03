e942be59fe167d433993794d86c2ae5af6d7cd55
David Zelenay <david.zelenay@tietoevry.com>
2021-01-03 14:16:11 +0100

    Removed CardDeck

--- swe-mtcg/Player/IPlayer.cs
+++ swe-mtcg/Player/IPlayer.cs
@@ -1,7 +1,5 @@
 ﻿using swe_mtcg.Card;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace swe_mtcg.Player
 {
@@ -9,9 +7,8 @@ namespace swe_mtcg.Player
     {
         public string Username { get; }
         public List<ICard> CardList { get; }
-        public float Elo { get; }
-        public ICardDeck ActiveDeck { get; }
-        public float Coins { get;  }
+        public double Elo { get; }
+        public double Coins { get;  }
         public bool AddCardToActiveDeck(int idxOfCard);
         public ICard TradeCard(int idxOfCard);
         public void AddCardToCardList(ICard card);
